4744355f08fafd7a898aef494bef54219d9b5e54
a31nesta <anthroponesta@gmail.com>
2025-04-01 17:46:13 +0200

    implement Grand

--- src/pages/libraries/grand.tsx
+++ src/pages/libraries/grand.tsx
@@ -1,15 +1,69 @@
-import { useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import BONav from "../../common/navbar";
+import initSync, { expr, GrandEx } from "../../grand/grand";
 
 function GRandWebsite() {
     const [resultOk, setResultOk] = useState(true);
     const [result, setResult] = useState("Enter an expression, the result will be here");
+    const [wasmReady, setWasmReady] = useState(false);
+    const [expression, setExpression] = useState("");
 
-    function generate(formData: FormData) {
-        const expression = formData.get("expression");
-        if (expression) {
-            setResult(expression.toString());
-            setResultOk(!resultOk);
+    const [compiledGrandEx, setCompiledGrandEx] = useState<GrandEx | null>(null);
+    const [compiledStringEx, setCompiledStringEx] = useState("");
+
+    useEffect(()=>{
+        initSync()
+            .then(()=>{
+                setWasmReady(true);
+            })
+            .catch((_)=>{
+                setResult("Couldn't load Grand");
+                setResultOk(false);
+            })
+    });
+
+    function generate(event: FormEvent) {
+        event.preventDefault();
+        // If we're still waiting for WASM we show an error message and return
+        if (!wasmReady) {
+            setResult("Grand is not loaded");
+            setResultOk(false);
+            return;
+        }
+
+        // Show result
+        let gex: GrandEx;
+        if (expression != compiledStringEx) {
+            try {
+                gex = expr(expression.toString());
+
+                // Free previous GrandExpr
+                if (compiledGrandEx) {
+                    compiledGrandEx.free()
+                }
+                // Set new GrandExpr
+                setCompiledGrandEx(gex);
+                setCompiledStringEx(expression);
+            } catch (_) {
+                setResult("Compilation error from Gex. Error reporting is still a work in progress");
+                setResultOk(false);
+                return;
+            }
+        } else if (compiledGrandEx) {
+            gex = compiledGrandEx;
+        } else {
+            setResult("Logically impossible error. Enjoy this goofy ahh message!");
+            setResultOk(false);
+            return;
+        }
+
+        try {
+            let res = gex.eval();
+            setResult(res.toString());
+            setResultOk(true);
+        } catch (_) {
+            setResult("Runtime error from Gex Error reporting is still a work in progress");
+            setResultOk(false);
         }
     }
 
@@ -26,9 +80,9 @@ function GRandWebsite() {
                 <div className="flex flex-col md:flex-row gap-4 w-full">
                     <div className="p-4 border border-fuchsia-300 rounded-2xl md:w-full">
                         <h2 className="text-xl mb-4">Expression</h2>
-                        <form action={generate}>
+                        <form onSubmit={generate}>
                             <div className="flex flex-row gap-2">
-                                <input type="text" name="expression" className="p-2 bg-violet-300 font-mono text-fuchsia-950 rounded-xl grow" />
+                                <input type="text" value={expression} onChange={(e)=>{setExpression(e.target.value)}} className="p-2 bg-violet-300 font-mono text-fuchsia-950 rounded-xl grow" />
                                 <button type="submit" className="p-2 bg-violet-900 rounded-xl">Run</button>
                             </div>
                         </form>
